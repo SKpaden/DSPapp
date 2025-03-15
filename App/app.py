7910970e0b8340b107bccb28115eb19c91b1d1fd
SKpaden <finn-luca.nannt@gmx.de>
2025-03-15 23:20:19 +0100

    Added example data and trying out dynamic visualization.

--- App/app.py
+++ App/app.py
@@ -1,20 +1,34 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
+import pandas as pd
+import plotly.express as px
+
+# Load data:
+df = pd.read_csv("data\cleaned_BFL_data.csv")
+df['date'] = pd.to_datetime(df['date'])
 
 app = Dash(__name__)
 app.layout = html.Div(
     children=[
-        html.H1("My Enhanced Website", style={"textAlign": "center", "color": "teal"}),
+        html.H1("My Enhanced Website", style={"textAlign": "center"}),
         html.P("This is a paragraph with a description."),
         dcc.Input(placeholder="Type something...", style={"marginRight": "10px"}),
         html.Button("Click Me", id="button"),
-        dcc.Graph(
-            id="example-graph",
-            figure={
-                "data": [{"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "Sample"}],
-                "layout": {"title": "Example Graph"}
-            }
+        dcc.Dropdown(
+            id="dropdown",
+            options=[
+            {"label": ['skydives', 'base_jumps'], "value": ['skydives', 'base_jumps']}
+        ],
+        value=df.columns[1]
         )
     ],
     style={"padding": "20px"}
 )
+
+@app.callback(
+    Output("dynamic-graph", "figure"),
+    Input("dropdown", "value")
+)
+def update_graph(selected_column):
+    return px.line(df, x="date", y=selected_column, title=f"Line Chart of {selected_column}")
+
 server = app.server  # Expose the server for Render
